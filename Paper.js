63091e0c73c7468da47a70fdd9708ec6f2352050
Deep112211 <72795614+Deep112211@users.noreply.github.com>
2020-11-10 15:22:27 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,29 @@
+class Paper{
+    constructor(x, y, radius){
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            dencity:1.2
+        }
+
+        this.body = Bodies.circle(x, y, radius, options);
+
+        this.radius = radius;
+
+        this.x = x;
+        this.y = y;
+        
+        World.add(world, this.body);
+
+        
+    }
+
+    display() {
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        ellipseMode(RADIUS);
+        ellipse(0, 0, this.radius, this.radius);
+        pop();
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,67 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ground, groundsprite;
+
+var trashball;
+
+var ground;
+
+var box1, box2, box3;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1200, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+	ground = new Ground(600, 690, 1200, 20);
+
+	trashball = new Paper(200, 500, 10);
+
+	box1 = new Box(1000, 675, 100, 10);
+	box2 = new Box(945, 655, 10, 50);
+	box3 = new Box(1050, 630, 10, 100);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  background(0);
+
+  trashball.display();
+
+  ground.display();
+
+  box1.display();
+  box2.display();
+  box3.display();
+  
+  keypressed();
+  drawSprites();
+ 
+}
+
+function keypressed() {
+	if (keyCode === UP_ARROW) {
+		Matter.Body.applyForce(trashball.body, trashball.body.position, {x:0.0007, y:-0.0007})
+	}
+
+	if (keyCode === DOWN_ARROW) {
+		Matter.Body.applyForce(trashball.body, trashball.body.position, {x:0, y:-0})
+	}
+}
+
